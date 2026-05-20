1ca090a863ab942c824a1311ea089665c9cbb3b9
username29800 <piapia78490@gmail.com>
2026-05-20 14:58:36 +0900

    dev: replace cut with sed pattern-matching and substitution

--- dwell/src/lib.sh
+++ dwell/src/lib.sh
@@ -4,14 +4,17 @@ alias ldwell_print_alias="$ldwell_print_alias"
 ldwell_config_list='cat "$ldwell_config_lfile"'
 alias ldwell_config_list="$ldwell_config_list"
 
-ldwell_current_config='ldwell_config_list | grep '\"' *$ldwell_config_file$'\"' | cut -d" " -f2'
+ldwell_config_list_is='read ldwell_config_lfile'
+alias ldwell_config_list_is="$ldwell_config_list_is"
+
+ldwell_current_config='ldwell_config_list | grep '\"' *$ldwell_config_file$'\"' | sed '\''s,^[^ ]* *,,'\'''
 alias ldwell_current_config="$ldwell_current_config"
 
 ldwell_load_config='. "$ldwell_config_file"'
 alias ldwell_load_config="$ldwell_load_config"
 
 ldwell_update_config='
-ldwell_config_file="$(ldwell_config_list | grep "^$ldwell_config_alias " | cut -d" " -f2)"'
+ldwell_config_file="$(ldwell_config_list | grep "^$ldwell_config_alias " | sed '\''s,^[^ ]* *,,'\'')"'
 #alias ldwell_update_config="ldwell_config_file="\""$(eval $ldwell_update_config)"\"""
 alias ldwell_update_config="$ldwell_update_config"
 
